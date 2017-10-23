2a01935dbdff5bd570905daf25d9905b2d140798
Linda Ho <linda.oanh.ho@gmail.com>
2017-10-22 20:30:46 -0500

    Completed TODO items  FixedArrayQueue

--- src/main/java/edu/luc/cs271/arrayqueue/FixedArrayQueue.java
+++ src/main/java/edu/luc/cs271/arrayqueue/FixedArrayQueue.java
@@ -1,7 +1,6 @@
 package edu.luc.cs271.arrayqueue;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class FixedArrayQueue<E> implements SimpleQueue<E> {
@@ -16,7 +15,11 @@ public class FixedArrayQueue<E> implements SimpleQueue<E> {
 
   private final E[] data;
 
-  // TODO why do we need an explicit constructor?
+  private List<E> dataList;
+
+  // Done why do we need an explicit constructor?
+  // We need an explicit constructor to initialize the queue with a fixed capacity.
+  // With a fixed capacity, we will also determine what the rear index is.
 
   @SuppressWarnings("unchecked")
   public FixedArrayQueue(final int capacity) {
@@ -29,26 +32,46 @@ public class FixedArrayQueue<E> implements SimpleQueue<E> {
 
   @Override
   public boolean offer(final E obj) {
-    // TODO
+    // Done
+    if (size < capacity) {
+      size++;
+      rear = (rear + 1) % capacity;
+      data[rear] = obj;
+      return true;
+    }
     return false;
   }
 
   @Override
   public E peek() {
-    // TODO
-    return null;
+    // Done
+    if (size == 0) {
+      return null;
+    } else {
+      return data[front];
+    }
   }
 
   @Override
   public E poll() {
-    // TODO
-    return null;
+    // Done
+    if (size == 0) {
+      return null;
+    } else {
+      E result = data[front];
+      front = (front + 1) % capacity;
+      size--;
+      return result;
+    }
   }
 
   @Override
   public boolean isEmpty() {
-    // TODO
-    return true;
+    // Done
+    if (size == 0) {
+      return true;
+    }
+    return false;
   }
 
   @Override
@@ -58,7 +81,13 @@ public class FixedArrayQueue<E> implements SimpleQueue<E> {
 
   @Override
   public List<E> asList() {
-    // TODO implement using an ArrayList preallocated with the right size
-    return Arrays.asList();
+    // Done implement using an ArrayList preallocated with the right size
+    dataList = new ArrayList<>(capacity);
+    while (!isEmpty()) {
+      dataList.add(data[front]);
+      front = (front + 1) % capacity;
+      size--;
+    }
+    return dataList;
   }
 }
